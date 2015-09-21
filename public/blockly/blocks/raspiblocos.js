733da18aa2261b2533a0c90cda302ecfc7ac07d2
Eduarth Heinen <eduartheinen@gmail.com>
2015-09-21 16:41:33 -0300

    added raspiblocos' blocks and javascript generators to blockly folder

---
+++ public/blockly/blocks/raspiblocos.js
@@ -0,0 +1,152 @@
+var opt = [["2", "2"], ["3", "3"], ["4", "4"], ["7", "7"], ["8", "8"], ["9", "9"], ["10", "10"], ["11", "11"], ["14", "14"], ["15", "15"], ["17", "17"], ["18", "18"], ["22", "22"], ["23", "23"], ["24", "24"], ["25", "25"]];
+
+//GPIO
+Blockly.Blocks['gpio'] = {
+  init: function() {
+    this.setHelpUrl('http://www.example.com/');
+    this.setColour(180);
+    this.appendDummyInput()
+        .appendField("gpio");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown(opt), "number");
+    this.appendValueInput("instruction");
+    this.setInputsInline(true);
+    this.setOutput(true);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['turnon'] = {
+  init: function() {
+    this.setHelpUrl('http://www.example.com/');
+    this.setColour(180);
+    this.appendDummyInput()
+        .appendField("ligar");
+    this.setInputsInline(true);
+    this.setOutput(true);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['turnoff'] = {
+  init: function() {
+    this.setHelpUrl('http://www.example.com/');
+    this.setColour(180);
+    this.appendDummyInput()
+        .appendField("desligar");
+    this.setInputsInline(true);
+    this.setOutput(true);
+    this.setTooltip('');
+  }
+};
+
+Blockly.Blocks['getstate'] = {
+  init: function() {
+    this.setHelpUrl('http://www.example.com/');
+    this.setColour(180);
+    this.appendDummyInput()
+        .appendField("verificar estado");
+    this.setInputsInline(true);
+    this.setOutput(true);
+    this.setTooltip('');
+  }
+};
+
+//DC-MOTOR
+Blockly.Blocks['dcmotor'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("motor dc");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown(opt), "gpio1")
+        .appendField(new Blockly.FieldDropdown(opt), "gpio2")
+    this.appendValueInput("instruction");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setInputsInline(true);
+    this.setColour(260);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['moveforward'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("mover adiante");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldTextInput("100"), "time")
+        .appendField("ms");
+    this.setInputsInline(true);
+    this.setOutput(true);
+    this.setColour(260);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['movebackward'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("mover atrás");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldTextInput("100"), "time")
+        .appendField("ms");
+    this.setInputsInline(true);
+    this.setOutput(true);
+    this.setColour(260);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['twodcmotor'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("motores dc");
+    this.appendDummyInput()
+        .appendField("motor1")
+        .appendField(new Blockly.FieldDropdown(opt), "gpio1")
+        .appendField(new Blockly.FieldDropdown(opt), "gpio2");
+    this.appendDummyInput()
+        .appendField("motor2")
+        .appendField(new Blockly.FieldDropdown(opt), "gpio3")
+        .appendField(new Blockly.FieldDropdown(opt), "gpio4");
+    this.appendValueInput("instruction");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setColour(260);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+//sensor
+Blockly.Blocks['sensor'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("sensor de proximidade");
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown(opt), "gpio1")
+        .appendField(new Blockly.FieldDropdown(opt), "gpio2")
+    this.appendValueInput("instruction");
+    this.setPreviousStatement(true);
+    this.setNextStatement(true);
+    this.setInputsInline(true);
+    this.setColour(200);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
+
+Blockly.Blocks['measure'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("medir distância");
+    this.setInputsInline(true);
+    this.setOutput(true);
+    this.setColour(200);
+    this.setTooltip('');
+    this.setHelpUrl('http://www.example.com/');
+  }
+};
---
+++ public/blockly/generators/javascript/raspiblocos.js
@@ -0,0 +1,68 @@
+//gpio
+Blockly.JavaScript['gpio'] = function(block) {
+  var dropdown_number = block.getFieldValue('number');
+  var value_instruction = Blockly.JavaScript.valueToCode(block, 'instruction', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = "yield builder.gpio(" + dropdown_number + ")" + value_instruction;
+  return code;
+};
+
+Blockly.JavaScript['turnon'] = function(block) {
+  var code = '.turnOn()';
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+};
+
+Blockly.JavaScript['turnoff'] = function(block) {
+  var code = '.turnOff()';
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+};
+
+Blockly.JavaScript['getstate'] = function(block) {
+  var code = '.getState()';
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+};
+
+//dc-motor
+Blockly.JavaScript['dcmotor'] = function(block) {
+  var gpio1 = block.getFieldValue('gpio1');
+  var gpio2 = block.getFieldValue('gpio2');
+  var instruction = Blockly.JavaScript.valueToCode(block, 'instruction', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = "yield builder.dcmotor([" + gpio1 + "," + gpio2 + "])" + instruction + ";";
+  return code;
+};
+
+Blockly.JavaScript['moveforward'] = function(block) {
+  var time = block.getFieldValue('time');
+  var code = '.moveForward('+ time +')';
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+};
+
+Blockly.JavaScript['movebackward'] = function(block) {
+  var time = block.getFieldValue('time');
+  var code = '.moveBackward('+ time +')';
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+};
+
+Blockly.JavaScript['twodcmotor'] = function(block) {
+  var gpio1 = block.getFieldValue('gpio1');
+  var gpio2 = block.getFieldValue('gpio2');
+  var gpio3 = block.getFieldValue('gpio3');
+  var gpio4 = block.getFieldValue('gpio4');
+  var instruction = Blockly.JavaScript.valueToCode(block, 'instruction', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = "builder.dcmotor([" + gpio1 + "," + gpio2 + "])" + instruction;
+  var code = code + "; yield builder.dcmotor([" + gpio3 + "," + gpio4 + "])" + instruction + ";";
+  return code;
+};
+
+//sensor
+Blockly.JavaScript['sensor'] = function(block) {
+  var gpio1 = block.getFieldValue('gpio1');
+  var gpio2 = block.getFieldValue('gpio2');
+  var instruction = Blockly.JavaScript.valueToCode(block, 'instruction', Blockly.JavaScript.ORDER_ATOMIC);
+  var code = "yield builder.sensor([" + gpio1 + "," + gpio2 + "])" + instruction + ";";
+  return code;
+};
+
+Blockly.JavaScript['measure'] = function(block) {
+  var code = '.measure()';
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+};
